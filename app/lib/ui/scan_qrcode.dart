060e6b36d229717525516aa6a83c610443e26243
Mário Monteiro <marioammonteiro@gmail.com>
2022-03-09 22:00:27 +0000

    added qrcode scanner

--- app/lib/ui/scan_qrcode.dart
+++ app/lib/ui/scan_qrcode.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import '../app/app_state.dart';
 
@@ -9,6 +11,8 @@ class ScanQRCode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context, listen: false);
+    final cameraController = MobileScannerController();
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -18,16 +22,55 @@ class ScanQRCode extends StatelessWidget {
           style: TextStyle(
               fontSize: 20, fontWeight: FontWeight.w500, color: Colors.white),
         ),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('wip'),
-            ],
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
           ),
-        ),
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.cameraFacingState,
+              builder: (context, state, child) {
+                switch (state as CameraFacing) {
+                  case CameraFacing.front:
+                    return const Icon(Icons.camera_front);
+                  case CameraFacing.back:
+                    return const Icon(Icons.camera_rear);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.switchCamera(),
+          ),
+        ],
+      ),
+      body: MobileScanner(
+        controller: cameraController,
+        onDetect: (barcode, args) {
+          final code = barcode.rawValue;
+          print('Barcode found! $code');
+          // debugPrint('Barcode found! $code');
+          Fluttertoast.showToast(
+            msg: '$code',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.grey,
+          );
+        },
       ),
     );
   }
